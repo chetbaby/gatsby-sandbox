036017502559f508a9438657bd5776d7a4679993
Chet Hay <chet.hay@rosedigital.co>
2020-09-01 14:48:27 -0400

    changed some text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>CBABY's Gatsby Site</h1>
+    <p>This is all there is. Sad.</p>
+    <p>Covid has destroyed my body. Please dial for help.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
